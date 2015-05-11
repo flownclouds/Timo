9d9781a36670d479e7183d8e48a8f58cad3d4cfa
liuhuanting <i@liu.fm>
2015-05-11 20:39:05 +0800

    add murmurhash

--- src/main/server/re/ovo/timo/util/StringUtil.java
+++ src/main/server/re/ovo/timo/util/StringUtil.java
@@ -18,6 +18,8 @@ package re.ovo.timo.util;
 
 import java.io.ByteArrayOutputStream;
 import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.Random;
 
 /**
@@ -51,6 +53,38 @@ public class StringUtil {
             h = (h << 5) - h + s.charAt(i);
         }
         return h;
+    }
+    
+    public static long hash(String key) {
+        ByteBuffer buf = ByteBuffer.wrap(key.getBytes());
+        int seed = 0x1234ABCD;
+        ByteOrder byteOrder = buf.order();
+        buf.order(ByteOrder.LITTLE_ENDIAN);
+        long m = 0xc6a4a7935bd1e995L;
+        int r = 47;
+        long h = seed ^ (buf.remaining() * m);
+        long k;
+        while (buf.remaining() >= 8) {
+            k = buf.getLong();
+            k *= m;
+            k ^= k >>> r;
+            k *= m;
+            h ^= k;
+            h *= m;
+        }
+        if (buf.remaining() > 0) {
+            ByteBuffer finish = ByteBuffer.allocate(8).order(ByteOrder.LITTLE_ENDIAN);
+            // for big-endian version, do this first:
+            // finish.position(8-buf.remaining());
+            finish.put(buf).rewind();
+            h ^= finish.getLong();
+            h *= m;
+        }
+        h ^= h >>> r;
+        h *= m;
+        h ^= h >>> r;
+        buf.order(byteOrder);
+        return h & Long.MAX_VALUE;
     }
 
     public static byte[] encode(String src, String charset) {
